94225d960c99881551cdc861aa62c8b11b35d0fd
Hugo Fonseca <hugo.fonseca@jumia.com>
2018-01-30 19:16:19 +0000

    Updated README.md

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # webhook-demultiplexer
 
-REST API, developed in Go, to provide a translate layer between Status Cake webhooks and a given application's API.
+**webhook-demultiplexer** is a REST API, developed in Go, to provide a translation layer between StatusCake webhooks and a given application's API. It only exposes only one endpoint &#8212; configured as a webhook in StatusCake &#8212; and all its data processing is focused in HTTP payloads received from StatusCake requests. 
+
+At the moment, it only supports the following applications' API:
+
+- [Cachet](https://github.com/CachetHQ/Cachet): StatusCake creates or updates a given incident in Cachet
 
 ## Getting Started
+
+By default, this application runs at the top of a Docker container and its configured using variables read as environment variables or from Vault.
+
+However, it's still possible to run it without a container &#8212; let me call this **standalone** mode.
+
+### Standalone
+
+### Docker
--- app/hooks/cachet.go
+++ app/hooks/cachet.go
@@ -8,15 +8,17 @@ import (
 	"github.com/hugomcfonseca/cachet"
 )
 
+var cachetClient cachet.Client
+
 func main() {
-	client, err := cachet.NewClient("https://cachet.localhost", nil) // provide URL from cmdline
+	cachetClient, err := cachet.NewClient("https://cachet.localhost", nil) // provide URL from cmdline
 
 	if err != nil {
 		fmt.Printf("Error creating Cachet client: %s", err)
 		return
 	}
 
-	_, resp, err := client.General.Ping()
+	_, resp, err := cachetClient.General.Ping()
 
 	if resp.Status != "200" {
 		fmt.Printf("Cachet server is not reachable: %s", err)
@@ -24,4 +26,25 @@ func main() {
 	}
 }
 
-func 
+func tagsToComponents() (string, bool) {
+	componentResp, resp, err := cachetClient.Components.GetAll()
+
+	if err != nil {
+		return "", false
+	}
+
+	return "", true
+}
+
+func reportIncident(name string, description string, status int, compId string, compStatus int) {
+	component := cachet.Incident{
+		Name:        name,
+		Description: description,
+		Status:      cachet.ComponentStatusOperational, // fixes me
+		Visible: 	 cachet.IncidentVisibilityPublic,
+		ComponentID: compId,
+		ComponentStatus: cachet.ComponentStatusPartialOutage // fixes me
+	}
+
+	incidentResp, resp, err := cachetClient.Incidents.Create()
+}
